9a661e7591434886ff37d0e382e423fd1fc0885b
root <root@e5e5157d7fb9.(none)>
2015-05-20 11:24:12 -0400

    dummy_collector f -> c

--- collectors/dummy_collector.go
+++ collectors/dummy_collector.go
@@ -18,41 +18,41 @@ func NewDummyCollector(name string, interval time.Duration, points int) newcore.
 	if points <= 0 {
 		points = 1
 	}
-	f := &dummy_collector{
+	c := &dummy_collector{
 		name:     name,
 		enabled:  true,
 		interval: interval,
 		points:   points,
 	}
-	return f
+	return c
 }
 
-func (f *dummy_collector) Name() string {
-	return f.name
+func (c *dummy_collector) Name() string {
+	return c.name
 }
 
-func (f *dummy_collector) Close() error {
+func (c *dummy_collector) Close() error {
 	return nil
 }
 
-func (f *dummy_collector) ClassName() string {
+func (c *dummy_collector) ClassName() string {
 	return "dummy_collector"
 }
 
-func (f *dummy_collector) IsEnabled() bool {
-	return f.enabled
+func (c *dummy_collector) IsEnabled() bool {
+	return c.enabled
 }
 
-func (f *dummy_collector) Interval() time.Duration {
-	return f.interval
+func (c *dummy_collector) Interval() time.Duration {
+	return c.interval
 }
 
-func (f *dummy_collector) CollectOnce() *newcore.CollectResult {
+func (c *dummy_collector) CollectOnce() *newcore.CollectResult {
 	var items newcore.MultiDataPoint
 
-	for i := 0; i < f.points; i++ {
+	for i := 0; i < c.points; i++ {
 		items = append(items, &newcore.DataPoint{
-			Metric:    newcore.Metric(fmt.Sprintf("metric.%s", f.name)),
+			Metric:    newcore.Metric(fmt.Sprintf("metric.%s", c.name)),
 			Timestamp: time.Now(),
 			Value:     1,
 			Tags:      nil,
@@ -62,7 +62,7 @@ func (f *dummy_collector) CollectOnce() *newcore.CollectResult {
 
 	return &newcore.CollectResult{
 		Collected: &items,
-		Next:      time.Now().Add(f.interval),
+		Next:      time.Now().Add(c.interval),
 		Err:       nil,
 	}
 }
