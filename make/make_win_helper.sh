095b1fafd2693a8cdac9fdbb71acf7ff5e3b7d6a
oliveagle <oliveagle@gmail.com>
2015-06-03 13:41:12 +0800

    make/make_win_helper.sh

--- make/make_win_helper.sh
+++ make/make_win_helper.sh
@@ -9,5 +9,5 @@ SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 APP_ROOT="$SCRIPT_ROOT/.."
 
-cd "$APP_ROOT/misc/win_helper_service"
+cd "$APP_ROOT/.misc/win_helper_service"
 gcc -Os hickwall_helper.c -o hickwall_helper.exe && mv hickwall_helper.exe "$APP_ROOT/bin/"
