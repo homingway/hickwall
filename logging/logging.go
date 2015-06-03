961e85dbfe7f805b312600614ba4eee1e7ea814b
oliveagle <oliveagle@gmail.com>
2015-06-03 15:25:31 +0800

    fix: logging.go logger can be nil

--- logging/logging.go
+++ logging/logging.go
@@ -88,61 +88,61 @@ func SetLevel(lvl string) error {
 }
 
 func Trace(v ...interface{}) {
-	if _level <= level.TRACE {
+	if _level <= level.TRACE && logger != nil {
 		logger.Output(2, fmt.Sprintf("[TRACE] %s", v...))
 	}
 }
 
 func Tracef(format string, v ...interface{}) {
-	if _level <= level.TRACE {
+	if _level <= level.TRACE && logger != nil {
 		logger.Output(2, fmt.Sprintf("[TRACE] %s", fmt.Sprintf(format, v...)))
 	}
 }
 
 func Debug(v ...interface{}) {
-	if _level <= level.DEBUG {
+	if _level <= level.DEBUG && logger != nil {
 		logger.Output(2, fmt.Sprintf("[DEBUG] %s", v...)) // 1728 ns/op
 	}
 }
 
 func Debugf(format string, v ...interface{}) {
-	if _level <= level.DEBUG {
+	if _level <= level.DEBUG && logger != nil {
 		logger.Output(2, fmt.Sprintf("[DEBUG] %s", fmt.Sprintf(format, v...))) //2046 ns/op
 	}
 }
 
 func Info(v ...interface{}) {
-	if _level <= level.INFO {
+	if _level <= level.INFO && logger != nil {
 		logger.Output(2, fmt.Sprintf("[INFO] %s", v...))
 	}
 }
 
 func Infof(format string, v ...interface{}) {
-	if _level <= level.INFO {
+	if _level <= level.INFO && logger != nil {
 		logger.Output(2, fmt.Sprintf("[INFO] %s", fmt.Sprintf(format, v...)))
 	}
 }
 
 func Error(v ...interface{}) {
-	if _level <= level.ERROR {
+	if _level <= level.ERROR && logger != nil {
 		logger.Output(2, fmt.Sprintf("[ERROR] %s", v...))
 	}
 }
 
 func Errorf(format string, v ...interface{}) {
-	if _level <= level.ERROR {
+	if _level <= level.ERROR && logger != nil {
 		logger.Output(2, fmt.Sprintf("[ERROR] %s", fmt.Sprintf(format, v...)))
 	}
 }
 
 func Critical(v ...interface{}) {
-	if _level <= level.CRITICAL {
+	if _level <= level.CRITICAL && logger != nil {
 		logger.Output(2, fmt.Sprintf("[CRITICAL] %s", v...))
 	}
 }
 
 func Criticalf(format string, v ...interface{}) {
-	if _level <= level.CRITICAL {
+	if _level <= level.CRITICAL && logger != nil {
 		logger.Output(2, fmt.Sprintf("[CRITICAL] %s", fmt.Sprintf(format, v...)))
 	}
 }
